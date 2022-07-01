578e32f4c8c02d520f770e7d40600c0cb98ccdab
574 <2659332353@qq.com>
2022-07-01 22:00:04 +0800

    tijiao

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+import store from './store.js'
+import axios from 'axios'
 
+
+// axios.defaults.withCredentials = false
+axios.defaults.baseURL = "http://47.96.72.124:8003/"
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
