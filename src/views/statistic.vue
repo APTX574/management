629aea65604fb34bbc0ada234b9442794670db0c
574 <2659332353@qq.com>
2022-07-04 10:01:20 +0800

    添加图表

--- src/views/statistic.vue
+++ src/views/statistic.vue
@@ -29,50 +29,14 @@ export default {
     };
   },
   mounted() {
-    axios.post(
-        "/get/sum", {}
-    ).then(data => {
-          data = data.data.data
-          let option = {
-            series: [
-              {
-                name: '访问来源',
-                type: 'pie',
-                radius: '55%',
-                data:data,
-                itemStyle: {
-                  emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                  },
-                  normal:{
-                    label:{
-                      show: true,
-                      //formatter: '{b} : {c} ({d}%)' //带当前图例名 + 百分比
-                      formatter: '{b} : \n{c}元 ({d}%)' //只要百分比
-                    },
-                    labelLine :{show:true}
-                  }
-                }
-              }
-            ]
-
-          }
-
-          // 2.创建图表
-          let chart = this.$echarts.init(this.$refs.myChart)
-
-          // 3，导入图表的配置
-          chart.setOption(option)
-
-          // 4添加窗口大小改变监听事件，当窗口大小改变时，图表会重新绘制，自适应窗口大小
-          window.addEventListener('resize', function () {
-            chart.resize()
-          })
-        }
-    )
+    this.getPie()
     // 1.配置图表的数据和参数
+    axios.post("/get/day/sum", {
+      year: 2022,
+      month: 7
+    }).then(data => {
+      console.log(data)
+    })
 
   },
   methods: {
@@ -112,15 +76,87 @@ export default {
 //         })
     },
 
+    getPie() {
+      axios.post(
+          "/get/sum", {}
+      ).then(data => {
+        data = data.data.data
+        let option = {
+          series: [
+            {
+              name: '访问来源',
+              type: 'pie',
+              radius: '55%',
+              data: data,
+              itemStyle: {
+                emphasis: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+                },
+                normal: {
+                  label: {
+                    show: true,
+                    //formatter: '{b} : {c} ({d}%)' //带当前图例名 + 百分比
+                    formatter: '{b} : \n{c}元 ({d}%)' //只要百分比
+                  },
+                  labelLine: {show: true}
+                }
+              }
+            }
+          ]
 
-    //     axios.post(
-    //     "/get/opt"
-    //     ,{
-    //       "id":"5"
-    //     }
-    // ).then(res=>{console.log(JSON.parse(res.request.response))
+        }
 
-    // }).catch(error=>{console.log(error)})
+        // 2.创建图表
+        let chart = this.$echarts.init(this.$refs.myChart)
+
+        // 3，导入图表的配置
+        chart.setOption(option)
+
+        // 4添加窗口大小改变监听事件，当窗口大小改变时，图表会重新绘制，自适应窗口大小
+        window.addEventListener('resize', function () {
+          chart.resize()
+        })
+      })
+    },
+    getDay() {
+      axios.post(
+          "/get/sum", {}
+      ).then(data => {
+        console.log(data)
+        data = data.data.data
+        let option = {
+          title: {
+            text: '每日支出'
+          },
+          tooltip: {},
+          legend: {
+            data: ['支出']
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
+        };
+
+        // 2.创建图表
+        let chart = this.$echarts.init(this.$refs.myChart)
+
+        // 3，导入图表的配置
+        chart.setOption(option)
+
+        // 4添加窗口大小改变监听事件，当窗口大小改变时，图表会重新绘制，自适应窗口大小
+        window.addEventListener('resize', function () {
+          chart.resize()
+        })
+      })
+    }
 
   },
 };
